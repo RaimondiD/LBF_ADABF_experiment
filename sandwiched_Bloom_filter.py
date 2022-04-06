d7c395c177584286d21469a8361949d190fef192
unknown <giacomo.fumagalli1@studenti.unimi.it>
2022-04-06 20:51:19 +0200

    Added control to the sum of FP and FN

--- sandwiched_Bloom_filter.py
+++ sandwiched_Bloom_filter.py
@@ -47,7 +47,7 @@ class SLBF(Abstract_Filter):
 def train_slbf(filter_size, query_train_set, keys, quantile_order):
     train_dataset = np.array(pd.concat([query_train_set, keys]).iloc[:, -1])
     thresholds_list = [np.quantile(train_dataset, i * (1 / quantile_order)) for i in range(1, quantile_order)] if quantile_order < len(train_dataset) else np.sort(train_dataset)
-    thresh_third_quart_idx = (3 * len(thresholds_list) - 1) // 4
+    # thresh_third_quart_idx = (3 * len(thresholds_list) - 1) // 4
 
     fp_opt = query_train_set.shape[0]
     slbf_opt = None #cambiare
@@ -60,6 +60,9 @@ def train_slbf(filter_size, query_train_set, keys, quantile_order):
         FN = (keys[ml_false_negative].iloc[:, 1].size) / keys.iloc[:, 1].size # stima probabilità di un falso negativo dal modello
 
         if (FP == 0.0 or FP == 1.0) or (FN == 1.0 or FN == 0.0): continue
+        if (FP + FN > 1): # If FP + FN >= 1, the budget b2 becomes negative
+            print("FP + FN >= 1")
+            continue
 
         b2 = FN * math.log(FP / ((1 - FP) * ((1/FN) - 1)), 0.6185)
         b1 = filter_size - b2
@@ -67,7 +70,7 @@ def train_slbf(filter_size, query_train_set, keys, quantile_order):
             print("b1 = 0")
             break
 
-        # print(f"FP: {FP}, FN: {FN}, b: {filter_size}, b1: {b1}, b2: {b2}")
+        print(f"FP: {FP}, FN: {FN}, b: {filter_size}, b1: {b1}, b2: {b2}")
 
         slbf = SLBF(keys, b1, b2, threshold)
         fp_items = slbf.query(query_train_set)
